f5e4933065129affbd5500602ac9a52e8ba81881
albertomontano <alberto_mp90@hotmail.com>
2025-04-28 22:25:16 -0600

    Creation of AssignmentLoops project

---
+++ src/main/java/Assignment1UsingForLoops.java
@@ -0,0 +1,14 @@
+public class Assignment1UsingForLoops {
+
+    public static void main(String[] args){
+
+        //Write a for loop that prints the numbers from 1 to 10.
+
+        //Write a for loop that calculates the sum of the first 10 positive integers and prints the result.
+
+        //Write a for loop that prints the multiplication table of a number provided by the user (from 1 to 10).
+
+    }
+
+
+}
---
+++ src/main/java/Assignment2UsingWhileLoops.java
@@ -0,0 +1,25 @@
+public class Assignment2UsingWhileLoops {
+
+    public static void main(String[] args) {
+
+        /*
+        Write a Java program that prompts the user to enter a positive integer.
+        Use a Scanner object to read user input.
+        */
+
+
+        /*
+        * Use a while loop to calculate the sum of the digits of the entered number.
+        Continuously extract the last digit of the number and add it to a running total.
+        Remove the last digit from the number using integer division.
+        */
+
+        //Once the sum of the digits has been calculated, print the result to the user.
+
+        /*
+        Add input validation to ensure the user enters a positive integer.
+        Allow the user to enter multiple numbers and calculate the sum of the digits for each one until they choose to exit.
+        */
+
+    }
+}
